8f80afaf5bb10881ac6ec8cf2525101d4d2b8558
Ian Gutwinski <iangutwinski@gmail.com>
2019-02-02 20:27:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,13 @@
 def roll_call_dwarves(array)
-  array.each_with_index {|dwarf, index| 
+  array.each_with_index {|dwarf, index|
     puts "#{index+1} #{dwarf}"
   }
 end
 
 def summon_captain_planet(array)
   array.collect do |phrase|
-     
-  end 
+
+  end
 end
 
 def long_planeteer_calls# code an argument here
